a09412ee139dbeaf80bbf5aa45e8b183cb767431
sumeet chandel <sumeetchandel321@gmail.com>
2022-06-01 07:50:39 +0530

    changes

--- dialogs/mainDialog.js
+++ dialogs/mainDialog.js
@@ -101,7 +101,6 @@ class MainDialog extends LogoutDialog {
     const tokenResponse = stepContext.result;
     if (tokenResponse) {
       await stepContext.context.sendActivity("You are now logged");
-      return await step.beginDialog(OAUTH_PROMPT);
     }
     await stepContext.context.sendActivity(
       "Login was not successful please try again."
